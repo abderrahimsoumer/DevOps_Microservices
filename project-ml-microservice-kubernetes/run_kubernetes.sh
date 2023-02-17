c85d77cc09f3d4850f1d900eb3654ddc331ea841
Abderrahim SOUMER <soumer120@gmail.com>
2023-02-17 18:41:04 +0000

    Deploy with Kubernete

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=remuos/ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-microservice-kubernetes \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=ml-microservice-kubernetes
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-microservice-kubernetes 8000:80
 
