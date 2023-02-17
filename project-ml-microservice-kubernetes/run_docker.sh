8abb7b89f3eda90463023ad88e4ee0c0b4f9954c
Abderrahim SOUMER <soumer120@gmail.com>
2023-02-17 18:00:53 +0000

    update tag

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Build image
-docker build --tag=api .
+docker build --tag=remuos/ml-microservice-kubernetes .
 
 # List docker images
 docker image ls
 
 # Run flask app
-docker run -p 8000:80 api
+docker run -p 8000:80 remuos/ml-microservice-kubernetes
