ba59b3b05f85c057892572175a1587d5955ee1de
Abderrahim SOUMER <soumer120@gmail.com>
2023-02-17 18:02:34 +0000

    Upload docker

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=remuos/ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=remuos
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
